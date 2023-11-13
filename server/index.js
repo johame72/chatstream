5f4ded0b3f541a68498f9f236093abad1f3a35c8
johame72 <johnhmerkel@gmail.com>
2023-11-13 10:55:59 -0600

    Updated App.js code using API Docs

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// server\index.js
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
--- src/FormattedText.js
+++ src/FormattedText.js
@@ -1,3 +1,4 @@
+// src\FormattedText.js
 import React from 'react';
 import { marked } from 'marked';
 
