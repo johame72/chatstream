0ebf8f6ad97dfe74d0673abfead9911d4181c76e
johame72 <johnhmerkel@gmail.com>
2023-11-13 11:54:14 -0600

    New App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,37 @@
-// src\App.js
 import React, { useState, useEffect } from 'react';
 import FormattedText from './FormattedText';
-function App() {
+
+const useChat = (apiUrl) => {
   const [inputText, setInputText] = useState('');
   const [streamedContent, setStreamedContent] = useState('');
-  const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001';
-function parseStreamedData(dataString) {
-  try {
-    // Accumulator for JSON data
-    let jsonDataAccumulator = '';
-    const lines = dataString.split('\n');
-    const parsedData = [];
-    lines.forEach(line => {
-      // Remove 'data: ' prefix and trim
-      line = line.replace(/^data: /, '').trim();
-      // Check if line indicates end of data stream
-      if (line === '[DONE]') {
-        return;
-      }
-      // Accumulate JSON data
-      jsonDataAccumulator += line;
-      // Try to parse the accumulated data
+  let incompleteData = ''; // To hold incomplete data chunks
+
+  const handleStreamedData = (dataString) => {
+    // Append any previously incomplete data
+    const completeData = incompleteData + dataString;
+    incompleteData = ''; // Reset incomplete data
+
+    const lines = completeData.split('\n');
+    lines.forEach((line, index) => {
+      line = line.replace(/^data: /, "").trim();
+      if (["", "[DONE]"].includes(line)) return;
+
       try {
-        const parsedJson = JSON.parse(jsonDataAccumulator);
-        // If parse is successful, reset the accumulator and add parsed JSON to parsedData
-        jsonDataAccumulator = '';
-        parsedData.push(parsedJson);
-      } catch {
-        // If JSON is incomplete, wait for more data (do not reset jsonDataAccumulator)
-      }
-    });
-    return parsedData;
-  } catch (error) {
-    console.error('Error parsing chunk:', error);
-    return [];
-  }
-}
-  useEffect(() => {
-    const eventSource = new EventSource(`${apiUrl}/stream`);
-    eventSource.onmessage = function(event) {
-      console.log("Raw data:", event.data); // Add this line
-      const parsedChunks = parseStreamedData(event.data);
-      parsedChunks.forEach(chunk => {
-        if (chunk.choices && chunk.choices.length > 0) {
-          const content = chunk.choices[0].delta.content;
+        const parsedLine = JSON.parse(line);
+        if (parsedLine.choices && parsedLine.choices.length > 0) {
+          const content = parsedLine.choices[0].delta.content;
           if (content) {
             setStreamedContent(currentContent => currentContent + content);
           }
         }
-      });
-    };
-    eventSource.onerror = function(event) {
-      console.error('EventSource failed:', event);
-    };
-    return () => {
-      eventSource.close();
-    };
-  }, [apiUrl]);
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-  };
+      } catch (e) {
+        // If parsing fails, assume data is incomplete
+        // Save it to append to the next chunk
+        incompleteData = lines.slice(index).join('\n');
+      }
+    });
+  }
+
   const handleSubmit = async () => {
     try {
       const response = await fetch(`${apiUrl}/send`, {
@@ -69,22 +41,47 @@ function parseStreamedData(dataString) {
         },
         body: JSON.stringify({ message: inputText }),
       });
+
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      setStreamedContent(''); // Reset the streamed content
+
       setInputText(''); // Clear the input
+      setStreamedContent(''); // Reset the streamed content
     } catch (error) {
-      console.error('There has been a problem with your fetch operation:', error);
+      console.error('Problem with fetch operation:', error);
     }
   };
+
+  useEffect(() => {
+    const eventSource = new EventSource(`${apiUrl}/stream`);
+    eventSource.onmessage = (event) => {
+      handleStreamedData(event.data);
+    };
+
+    eventSource.onerror = (event) => {
+      console.error('EventSource failed:', event);
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, [apiUrl]);
+
+  return { inputText, setInputText, streamedContent, handleSubmit };
+};
+
+function App() {
+  const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+  const { inputText, setInputText, streamedContent, handleSubmit } = useChat(apiUrl);
+
   return (
     <div className="App">
       <h1>Real-time Streaming with OpenAI and SSE</h1>
       <input 
         type="text" 
         value={inputText} 
-        onChange={handleInputChange}
+        onChange={(e) => setInputText(e.target.value)}
       />
       <button onClick={handleSubmit}>
         Send
@@ -96,4 +93,5 @@ function parseStreamedData(dataString) {
     </div>
   );
 }
+
 export default App;
